7ae1ee4321cf374eab9bb747c26309c3479f30c4
benoit lagrange <benoitlagrange38@hotmail.fr>
2019-01-30 15:06:21 +0100

    Correction erreur

--- src/class_benoit.ts
+++ src/class_benoit.ts
@@ -4,16 +4,16 @@ import "./ns_music";
 import "./ns_friends";
 
 //Création du menu Food
-let freshFood = new NS_Food.Food()
-let foodEntree = freshFood.fetch('entree');
+let freshFood =  new NS_Food.Food()
+let foodEntree = freshFood.fetch('entrees');
 let foodPlat = freshFood.fetch('plats');
 let foodDessert = freshFood.fetch('desserts');
-let myMenu = {'plat' : foodEntree[2],"entree" : foodPlat[5],"dessert": foodDessert[2]}
+let myMenu = {'entree' : foodEntree[1],"plat" : foodPlat[1],"dessert": foodDessert[2]}
 
 //Création de ma liste d'amis
 let friendslist = new NS_Friends.Friends()
-let myFriendHomme = friendslist.fetch('Homme')
-let myFriendFemme = friendslist.fetch('Homme')
+let myFriendHomme = friendslist.fetch('hommes')
+let myFriendFemme = friendslist.fetch('femmes')
 let myfriend = {"femme": myFriendFemme[1] ,"homme" : myFriendHomme[2] }
 
 let musicList = new NS_Music.Music()
