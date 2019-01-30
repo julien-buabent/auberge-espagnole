5e18a479b6d6307204f15d4903d2a715e3476f5b
Kévin Dupas <38907117+Elw0od@users.noreply.github.com>
2019-01-30 14:58:25 +0100

    commit kevin

---
+++ src/class_kevin.ts
@@ -0,0 +1,11 @@
+import Participant from './class_participant';
+import "./ns_food";
+
+export default class Kevin extends Participant {
+    constructor() {
+        super();
+
+        this.name = "Kevin";
+        this.music = "Hello World";
+    }
+}
