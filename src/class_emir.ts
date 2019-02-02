dd451a15c76c0d3b0bf7c06bced516b069a20583
Emir OZCELIK <e.ozcelik@lodecom.fr>
2019-02-02 12:08:42 +0100

    simplied my class. I've gotten rid of useless local attributes and variables

--- src/class_emir.ts
+++ src/class_emir.ts
@@ -1,71 +1,25 @@
-import Participant from './class_participant';
+import Participant, {food, friends} from './class_participant';
 import "./ns_food";
 import "./ns_music";
 import "./ns_friends";
-import {food, friends} from "./class_participant";
 import * as tools from "./tools";
 
 let foodList = new NS_Food.Food();
 let friendList = new NS_Friends.Friends();
 let musicList = new NS_Music.Music();
 
-let listHommes:string[] = friendList.fetch("hommes");
-let listFemmes:string[] = friendList.fetch("femmes");
-let listMusic:string[] = musicList.playlist();
-
 export default class Emir extends Participant{
-  private _entree:string;
-  private _plat:string;
-  private _dessert:string;
-  private _amiHomme:string;
-  private _amiFemme:string;
   
   constructor(){
     super();
     this.name = "Emir";
-    this.entree = foodList.fetchOne("entrees");
-    this.plat = foodList.fetchOne("plats");
-    this.dessert = foodList.fetchOne("desserts");
-    let myFood:food = {entree:this.entree, plat:this.plat, dessert:this.dessert};
-    this.food = myFood;
-    this.amiHomme = this.getRandomItem(listHommes);
-    this.amiFemme = this.getRandomItem(listFemmes);
-    let myFriends:friends = {homme:this.amiHomme, femme:this.amiFemme};
-    this.friends = myFriends;
-    this.music = this.getRandomItem(listMusic);
+    this.food = {entree:foodList.fetchOne("entrees"), plat:foodList.fetchOne("plats"), dessert:foodList.fetchOne("desserts")};
+    this.friends = {homme:this.getRandomItem(friendList.fetch("hommes")), femme:this.getRandomItem(friendList.fetch("femmes"))};
+    this.music = this.getRandomItem(musicList.playlist());
     this.declareReady();
   }
 
-  get entree():string{
-    return this._entree;
-  }
-  set entree(article:string){
-    this._entree = article;
-  }
-  get plat():string{
-    return this._plat;
-  }
-  set plat(article:string){
-    this._plat = article;
-  }
-  get dessert():string{
-    return this._dessert;
-  }
-  set dessert(article:string){
-    this._dessert = article;
-  }
-  get amiHomme():string{
-    return this._amiHomme;
-  }
-  set amiHomme(person:string){
-    this._amiHomme = person;
-  }
-  get amiFemme():string{
-    return this._amiFemme;
-  }
-  set amiFemme(person:string){
-    this._amiFemme = person;
-  }
+ 
   getRandomItem = (bucket:string[]):string => {
     let bucketLength = bucket.length;
     let randomElementIndex:number = tools.getRandomNumberBetween(0, bucketLength-1);
